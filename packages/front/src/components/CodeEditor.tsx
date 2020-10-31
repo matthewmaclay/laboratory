0b72a95f8221ec8bb23a3d704b4833dd7fdcb7f7
Матвейкин Сергей / Matveykin Sergey <sergey.matveykin@rt.ru>
2020-11-01 02:41:49 +0300

    add edtir final

--- packages/front/src/components/CodeEditor.tsx
+++ packages/front/src/components/CodeEditor.tsx
@@ -9,15 +9,29 @@ import "ace-builds/src-noconflict/theme-solarized_dark";
 import { Flex, Button, Box } from "bumbag";
 
 export interface EditorProps {
-    id: string;
+  id: string;
 }
 
 const URL = "/run-test";
 
-const CodeEditor: React.FC<EditorProps> = ({id}) => {
+const PRE_CODE = {
+  javascript: "function runTest(){\n\n}",
+  python: "function runTest(){\n}",
+};
+
+const CodeEditor: React.FC<EditorProps> = ({ id }) => {
   const [language, setLanguage] = useState("javascript");
+  const [wrongCode, setWrongCode] = useState(false);
+  const [syntaxError, setSyntaxError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
   const [code, setCode] = useState("");
   const submitTest = () => {
+    setLoading(true);
+    setSyntaxError(false);
+    setWrongCode(false);
+    setSuccess(false);
     fetch(URL, {
       method: "POST",
       body: JSON.stringify({
@@ -25,13 +39,21 @@ const CodeEditor: React.FC<EditorProps> = ({id}) => {
         language,
         script: code,
       }),
-    });
+    })
+      .then((r) => r.json())
+      .then((response) => {
+        if (response.error) setSyntaxError(response.error);
+        if (response.result == "False") setWrongCode(true);
+        if (response.result == "True") setSuccess(true);
+        setLoading(false);
+      });
   };
+ 
   return (
     <Flex height="100%" flexDirection="column">
-      <Box flexGrow="222">
+      <Box position="relative" flexGrow="222">
         <AceEditor
-          //   defaultValue={props.content}
+          defaultValue={PRE_CODE[language]}
           width="800px"
           height="100%"
           fontSize={16}
@@ -41,6 +63,21 @@ const CodeEditor: React.FC<EditorProps> = ({id}) => {
           name={"CODE EDITOR"}
           editorProps={{ $blockScrolling: true }}
         />
+        {success && (
+          <Box zIndex="121" width="100%" padding="6px 20px" fontSize="12px" position="absolute" bottom="0" background="#27AE6080">
+            Результат верный, можете перейти к след задаче
+          </Box>
+        )}
+        {syntaxError && (
+          <Box zIndex="121" width="100%" padding="6px 20px" fontSize="12px" position="absolute" bottom="0" background="#C8322766">
+            {syntaxError}
+          </Box>
+        )}
+        {wrongCode && (
+          <Box zIndex="121" width="100%" padding="6px 20px" fontSize="12px" position="absolute" bottom="0" background="#C8322766">
+            Результат неверный, проверьте ваше решение
+          </Box>
+        )}
       </Box>
 
       <Flex justifyContent="space-between" flexShrink="2" padding="18px 31px">
@@ -52,7 +89,8 @@ const CodeEditor: React.FC<EditorProps> = ({id}) => {
           onChange={({ target }) => setLanguage(target.value)}
           value={language}
         />
-        <Button onClick={submitTest} palette="primary">
+
+        <Button isLoading={loading} onClick={submitTest} palette="primary">
           ТЕСТ
         </Button>
       </Flex>
