599bc6e7a1a459003b071430729569092bf32b6a
Матвейкин Сергей / Matveykin Sergey <sergey.matveykin@rt.ru>
2020-10-31 20:03:31 +0300

    add tasks to teacher

--- packages/front/src/components/pages/teacher/index.tsx
+++ packages/front/src/components/pages/teacher/index.tsx
@@ -6,6 +6,7 @@ import {
   useCreateGroupMutation,
   useGetGroupsWhereIamTeacherQuery,
   useGetStudentsQuery,
+  useGetTasksByGroupIdQuery,
 } from "graphqlTypes";
 import { UserContext } from "components/providers/UserProvider";
 import AvatarWithName from "components/AvatarWithName";
@@ -29,7 +30,10 @@ const IndexTeacherPage: React.FC = ({ children }) => {
   const [titleNewGroup, setTitleNewGroup] = React.useState("");
   const { teacher, avatar, firstName, id } = useContext(UserContext);
   const { data: dataStudents } = useGetStudentsQuery();
-
+  const { data: dataTasks } = useGetTasksByGroupIdQuery({
+    variables: { groupId: activeGroup.id },
+  });
+  console.log(dataTasks)
   const {
     data,
     error,
@@ -41,8 +45,8 @@ const IndexTeacherPage: React.FC = ({ children }) => {
   const [createGroup] = useCreateGroupMutation({
     onCompleted: () => {
       refetchGroups();
-      setTitleNewGroup('')
-      setStudents(null)
+      setTitleNewGroup("");
+      setStudents(null);
     },
   });
   useEffect(() => {
@@ -117,7 +121,12 @@ const IndexTeacherPage: React.FC = ({ children }) => {
               {!!data.groups.length &&
                 data.groups.map((i) => (
                   <Link
-                    onClick={() => setActiveGroup({ id: i.id, title: i?.title || "Название не задано"  })}
+                    onClick={() =>
+                      setActiveGroup({
+                        id: i.id,
+                        title: i?.title || "Название не задано",
+                      })
+                    }
                     marginBottom="24px"
                     color={activeGroup?.id == i.id ? "white" : "#8883F1"}
                   >
@@ -129,7 +138,9 @@ const IndexTeacherPage: React.FC = ({ children }) => {
         </div>,
       ]}
     >
-      <Box width="100%" background="red">123</Box>
+      <Box width="100%" background="red">
+        123
+      </Box>
     </WithSideBar>
   );
 };
