f770199af3d7e0f0b1d96dc157868d1b7fb7c800
rytikovroman1994 <bboycooler@yandex.ru>
2020-10-31 14:55:54 +0300

    add component dashboard

---
+++ packages/front/src/components/dashboard/header.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import style from 'styled-components';
+import { Button } from 'bumbag';
+import Link from "next/link";
+
+const StyledLessonHeader = style.div`
+    display: flex;
+    justify-content: space-between;
+    height: 50px;
+    border-bottom: 2px solid #E0E0E0;
+
+    .name.selected {
+        color: #5454E2;
+    }
+`
+
+const StyledInput = style.div`
+    margin: 32px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 18px;      
+    color: #E0E4EA;
+    cursor: pointer;
+`
+
+const StyledBlockStatus = style.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const StyledBlockButton = style.div`
+
+`
+
+const  EditorHeader = ({ fun }) => {
+
+    const [choseType, setType] = useState();
+
+    function choseTypeReq(name) {
+        setType(name)
+    }
+
+    function handleClick(type: string) {
+        fun(type)
+    }
+    return(
+        <StyledLessonHeader>
+            <StyledBlockStatus> 
+                <StyledInput 
+                    className={`name ${choseType == 'Task' ? "selected" : ""}`}
+                    onClick={() => {
+                    choseTypeReq('Task')
+                    handleClick('Task')
+                    }}>
+                    Задания
+                </StyledInput>
+                <StyledInput 
+                    className={`name ${choseType == 'Stady' ? "selected" : ""}`}
+                    onClick={() => {
+                        choseTypeReq('Stady')
+                        handleClick('Stady')
+                    }}>
+                    Ученики
+                </StyledInput>
+            </StyledBlockStatus>
+            <Link href='/author'>  
+                <StyledBlockButton>
+                    <Button variant="ghost" color="#E0E4EA">
+                        Пригласить
+                    </Button>
+                    <Button palette="primary">
+                        + Создать задание
+                    </Button>
+                </StyledBlockButton>
+            </Link>
+        </StyledLessonHeader>
+    )
+}
+
+export default EditorHeader;
---
+++ packages/front/src/components/dashboard/index.tsx
@@ -0,0 +1,175 @@
+import React, { useState } from "react";
+import style from 'styled-components';
+import Link from "next/link";
+import EditorHeader from './header';
+import { Table } from 'bumbag'
+
+import { Flex, Box } from "bumbag";
+
+const StyledCourseList = style.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;	
+`
+
+const StyledHeader = style.div`
+	width: 80%;
+	margin-top: 47px;
+`
+
+const StyledCars = style.div`
+	margin-top: 64px;
+	width: 80%;
+	cursor: pointer;
+
+	.Card {
+		border: 1px solid #CBCFD3;
+	}
+	
+	.Card:hover {
+		background: #FCFCFD;
+		box-shadow: 1px 2px 6px rgba(0, 0, 0, 0.25);
+	}
+`
+
+const StyledLinks = style.div`
+	font-style: normal;
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 18px;
+	margin: 35px 62px 70px 13px;
+
+	color: #FFFFFF;
+`
+
+const StyledStatus = style.div`
+	font-style: normal;
+	font-weight: normal;
+	font-size: 13px;
+	line-height: 16px;
+	color: #FFFFFF;
+	margin-left: 13px;
+
+	data {
+		color: #27AE60
+	}
+`
+
+const StyledSeparator = style.div`
+	height: 33px;
+`
+
+const StyledTable = style.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+`
+
+const mock = {
+	lessons: [
+		{
+			id: 1,
+			title:'Задание 21',
+			time: '17 октября'
+		},
+		{
+			id: 1,
+			title:'Задание 22',
+			time: '17 октября'
+		},
+		{
+			id: 1,
+			title:'Задание 23',
+			time: '17 октября'
+		},
+		{
+			id: 1,
+			title:'Задание 24',
+			time: '17 октября'
+		},
+		{
+			id: 1,
+			title:'Задание 25',
+			time: '17 октября'
+		}
+	],
+	stady: [
+		{ name: 'Иван Иванов', point: '89' },
+		{ name: 'Иван Петров', point: '25' },
+		{ name: 'Иван Курочкин', point: '67' },
+		{ name: 'Иван Муравьёв', point: '47' }
+	]
+}
+
+export default function CourseList() {
+	const [choseType, setType] = useState("Task");
+
+	function handleClick(type: string) {
+		setType(type)
+	}
+
+	if(choseType == 'Task') {
+		return (
+			<StyledCourseList>
+				<StyledHeader>
+					<EditorHeader fun={handleClick}/>
+				</StyledHeader>
+				<StyledCars >
+					<Flex flexDirection="row" flexWrap="wrap">
+						{mock.lessons.map((i) => (
+							<Link href={`/author/lesson/${i.id}/description`}>
+								<Box width="240px" 
+									height="196px" 
+									margin="23px" 
+									background="#617083"
+									box-shadow="1px 2px 24px rgba(0, 0, 0, 0.12)"
+									borderRadius="8px"
+									className="Card"
+								>
+										<>
+											<StyledLinks>
+												{i.title}
+											</StyledLinks>
+											<StyledStatus>
+												Завершить <data>{i.time}</data>		
+											</StyledStatus>
+										</>
+								</Box>
+							</Link>
+						))}
+					</Flex>
+				</StyledCars>
+			</StyledCourseList>
+		);
+	}
+	if(choseType == 'Stady') { 
+		return(
+			<StyledCourseList>
+				<StyledHeader>
+					<EditorHeader fun={handleClick}/>
+				</StyledHeader>
+				<StyledSeparator />
+				<StyledTable>
+					<Table width="80%" color="#E0E4EA" >
+						<Table.Head background="#171C26">
+							<Table.Row>
+								<Table.HeadCell color="#E0E4EA">Имя ученика</Table.HeadCell>
+								<Table.HeadCell textAlign="right" color="#E0E4EA">Средний балл</Table.HeadCell>
+							</Table.Row>
+						</Table.Head>
+						<Table.Body color="#E0E4EA">
+							{mock.stady.map(item=> {
+								return(
+									<Table.Row>
+										<Table.Cell>{item.name}</Table.Cell>
+										<Table.Cell textAlign="right">{item.point}</Table.Cell>
+									</Table.Row>
+								)})}
+						</Table.Body>
+					</Table>
+				</StyledTable>
+			</StyledCourseList>
+		)
+	}
+}
