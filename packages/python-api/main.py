837ca642f0c12bc77f1e7030e8f9ee99b60886b7
Беристов Антон Владимирович <av_beristov@rgsbank.ru>
2020-10-31 20:14:20 +0300

    update app

--- packages/python-api/main.py
+++ packages/python-api/main.py
@@ -6,20 +6,25 @@ from pydantic import BaseModel
 import requests
 import json
 
-client = docker.from_env()
-#client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-client.images.build(path="./", dockerfile=".docker/javascript.docker", tag="platform:javascript")
-client.images.build(path="./", dockerfile=".docker/python.docker", tag="platform:python")
+#client = docker.from_env()
+client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+client.images.build(
+    path="./", dockerfile=".docker/javascript.docker", tag="platform:javascript")
+client.images.build(
+    path="./", dockerfile=".docker/python.docker", tag="platform:python")
 app = FastAPI()
+
 
 class Item(BaseModel):
     id: int
     language: str
     script: str
 
+
 @app.post("/check")
 async def script_check(item: Item):
-    response = requests.get(f'https://admin.hack.dokub.xyz/exercises/{item.id}').json()
+    response = requests.get(
+        f'https://admin.hack.dokub.xyz/exercises/{item.id}').json()
     result_values = []
     tests_scripts = ""
     try:
@@ -28,16 +33,20 @@ async def script_check(item: Item):
                 result_values.append(test["result"])
                 tests_scripts += f'print(runTest{test["args"][0]["value"], test["args"][1]["value"]})\n'
             formated_script = f'{item.script}\n{tests_scripts}'
-            res = client.containers.run(f'platform:{item.language}', f'"{formated_script}"').decode("utf-8").split()
-            json_res = jsonable_encoder({"result": f"{res == result_values}", "goodAnswer": result_values, "yourAnswer": res})
+            res = client.containers.run(
+                f'platform:{item.language}', f'"{formated_script}"').decode("utf-8").split()
+            json_res = jsonable_encoder(
+                {"result": f"{res == result_values}", "goodAnswer": result_values, "yourAnswer": res})
             return JSONResponse(content=json_res)
         elif item.language == "javascript":
             res = []
             for test in response["tests"]:
                 result_values.append(test["result"])
                 tests_scripts = f'({item.script}){test["args"][0]["value"], test["args"][1]["value"]};'
-                res.append(client.containers.run(f'platform:{item.language}', f'"{tests_scripts}"').decode("utf-8").rstrip())
-            json_res = jsonable_encoder({"result": f"{res == result_values}", "goodAnswer": result_values, "yourAnswer": res})
+                res.append(client.containers.run(
+                    f'platform:{item.language}', f'"{tests_scripts}"').decode("utf-8").rstrip())
+            json_res = jsonable_encoder(
+                {"result": f"{res == result_values}", "goodAnswer": result_values, "yourAnswer": res})
             return JSONResponse(content=json_res)
 
     except docker.errors.ContainerError as e:
